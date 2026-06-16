5cd0e8daa5fe040009a25e2a3106815582b7283d
drunkod <drunkod@gmail.com>
2026-06-16 13:01:44 +0500

    content observer

---
+++ src/content/observer.ts
@@ -0,0 +1,11 @@
+let wikiObserver: MutationObserver | null = null;
+let timer: ReturnType<typeof window.setTimeout> | undefined;
+
+export function observeWikiPage(callback: () => void): void {
+  wikiObserver?.disconnect();
+  wikiObserver = new MutationObserver(() => {
+    if (timer) window.clearTimeout(timer);
+    timer = window.setTimeout(callback, 600);
+  });
+  wikiObserver.observe(document.body, { childList: true, subtree: true });
+}
